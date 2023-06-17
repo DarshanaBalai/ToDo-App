88d638d41cd6fc176d071300e166fa675628b74f
Darshana Balai <darshana.balai@gmail.com>
2023-06-17 12:20:24 +0000

    Add divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,20 @@ const task = document.querySelector("#taskContainer");
 taskForm.addEventListener('submit', (e) => {
 e.preventDefault();
 
-const task-container = do
+const addedTask = inputTask.value;
+
+const task_container = document.createElement('div');
+task_container.classList.add('containerTask')
+
+const task_content = document.createElement('div');
+task_content.classList.add('taskContent');
+
+task_container.appendChild(task_container);
+
+
+
+
+
 }
+
+
