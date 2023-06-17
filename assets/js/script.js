d18be03adeb415aa208587a1392e321d1fd11f1d
Darshana Balai <darshana.balai@gmail.com>
2023-06-17 12:14:27 +0000

    Add variables in js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+const taskForm =document.querySelector("#form");
+const inputTask = document.querySelector(".addInput");
+const addTask = document.querySelector(".addSubmit");
+const task = document.querySelector("#taskContainer");
 
+taskForm.addEventListener('submit', (e) => {
+e.preventDefault();
 
+const task-container = do
+}
