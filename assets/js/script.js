1c3842c37129ac6a504f927a9522e3987150c482
Darshana Balai <darshana.balai@gmail.com>
2023-06-17 12:24:10 +0000

    Add input

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,15 @@ const task_container = document.createElement('div');
 task_container.classList.add('containerTask')
 
 const task_content = document.createElement('div');
-task_content.classList.add('taskContent');
+task_content.classList.add('task_Content');
 
 task_container.appendChild(task_container);
 
-
+const addedInput = document.createElement('input');
+addedInput.classList.add("added_input")
+addedInput.type = "text";
+addedInput.value = addedTask;
+addedInput.setAttribute('readonly', 'readonly');
 
 
 
